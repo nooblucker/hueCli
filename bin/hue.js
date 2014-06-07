668a0ebfd95affcae5c6cc8f84ed11f4f1fc04b3
Sven Riecker <sven@riecker.de>
2014-06-07 18:24:35 +0200

    can set bri and ct

--- bin/hue.js
+++ bin/hue.js
@@ -2,6 +2,7 @@
 var Operetta = require('../node-operetta/operetta').Operetta;
 var scenes = require('hue-scenes');
 var getHueApi = require('../index');
+var csscolors = require('css-color-names');
 
 operetta = new Operetta();
 
@@ -67,27 +68,63 @@ operetta.command('notify', 'flashes all lights once', function(command) {
   });
 });
 
-operetta.command('lights', 'set light state', function(command) {
-  command.parameters(['-c','--color'], "hex value or css name");
+operetta.command('lights', 'control single lights', function(command) {
+  command.options(['-l', '--list'], 'list all available lights');
+  command.options(['-C','--colors'], 'show available color names. but you can use hex values, if you are not happy with them.');
+  command.parameters(['-c','--color'], 'hex value or css name');
+  command.parameters(['-b','--brightness'], 'in range 0..255');
+  command.parameters(['-T','--temperature'], 'set temperature between [153, 500] = [6500K, 2000K]');
   command.start(function(opt) {
-    var csscolors = require('css-color-names');
 
-    function torgb(hex) {
-      var b = parseInt(hex.replace('#', ''),16);
-      return [b>>16, b>>8&255, b&255];
+    if (opt['-l']) {
+      // list all lights
+      getHueApi(function(api) {
+        api.lights().then(function(res) {
+          console.dir(res);
+        }).done(function() {
+          process.exit(0);
+        });
+      });
+    } else if (opt['-C']) {
+      // list all colors
+      for(var key in csscolors) {
+        if (csscolors.hasOwnProperty(key)) {
+          console.log(key);
+        }
+      }
+      process.exit(0);
+    } else {
+      // set state
+      var LightState = require('node-hue-api').lightState;
+      var state = LightState.create().on(true);
+      if (opt['-c']) {
+        var toRGB = function(hex) {
+          var b = parseInt(hex.replace('#', ''),16);
+          return [b>>16, b>>8&255, b&255];
+        };
+        var hex = csscolors[opt['-c']] || opt['-c'];
+        var rgb = toRGB(hex);
+        state = state.rgb(rgb[0], rgb[1], rgb[2]);
+      }
+      if (opt['-T']) {
+        state.ct = parseInt(opt['-T']);
+      }
+      if (opt['-b']) {
+        state.bri = parseInt(opt['-b']);
+      }
+
+      getHueApi(function(api) {
+        // no specific lights provided -> apply to all lights
+        if (opt.positional.length === 0) {
+          api.setGroupLightState('0', state).then(console.dir).done();
+        } else {
+          // apply to each provided light
+          for (var i = 0; i < opt.positional.length; ++i) {
+            api.setLightState(parseInt(opt.positional[i]), state).then(console.dir).done();
+          }
+        }
+      });
     }
-
-    var hex = csscolors[opt['-c']];
-    var rgb = torgb(hex);
-
-    var LightState = require('node-hue-api').lightState;
-    var state = LightState.create().on(true).rgb(rgb[0], rgb[1], rgb[2]);
-
-    getHueApi(function(api) {
-      api.setGroupLightState('0', state).then(function(res) {
-        console.dir(res);
-      }).done();
-    });
 
   });
 });
